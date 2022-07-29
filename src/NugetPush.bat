b22faaf450751b3123ff8e0206933ff179ac538c
Peter Kostov <p.kostov@erp.bg>
2022-07-29 13:48:14 +0300

    updated to version 22.1.0

--- src/NugetPush.bat
+++ src/NugetPush.bat
@@ -1,4 +1,4 @@
 
-dotnet nuget push ErpNet.Api.Client\bin\Debug\ErpNet.Api.Client.1.0.2.nupkg --api-key %1 --source https://api.nuget.org/v3/index.json
+dotnet nuget push ErpNet.Api.Client\bin\Debug\ErpNet.Api.Client.22.1.0.nupkg --api-key %1 --source https://api.nuget.org/v3/index.json
 
-dotnet nuget push ErpNet.Api.Client.DomainApi\bin\Debug\ErpNet.Api.Client.DomainApi.1.0.2.nupkg --api-key %1 --source https://api.nuget.org/v3/index.json
+dotnet nuget push ErpNet.Api.Client.DomainApi\bin\Debug\ErpNet.Api.Client.DomainApi.22.1.0.nupkg --api-key %1 --source https://api.nuget.org/v3/index.json
