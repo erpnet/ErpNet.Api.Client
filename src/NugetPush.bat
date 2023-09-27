32adb5d4a806380c1fb39334097f1a48b088e7ae
Peter Kostov <p.kostov@erp.bg>
2023-09-27 16:33:39 +0300

    v23.1.1

--- src/NugetPush.bat
+++ src/NugetPush.bat
@@ -1,4 +1,4 @@
 
-dotnet nuget push ErpNet.Api.Client\bin\Debug\ErpNet.Api.Client.23.1.0.nupkg --api-key %1 --source https://api.nuget.org/v3/index.json
+dotnet nuget push ErpNet.Api.Client\bin\Debug\ErpNet.Api.Client.23.1.1.nupkg --api-key %1 --source https://api.nuget.org/v3/index.json
 
-dotnet nuget push ErpNet.Api.Client.DomainApi\bin\Debug\ErpNet.Api.Client.DomainApi.23.1.0.nupkg --api-key %1 --source https://api.nuget.org/v3/index.json
+dotnet nuget push ErpNet.Api.Client.DomainApi\bin\Debug\ErpNet.Api.Client.DomainApi.23.1.1.nupkg --api-key %1 --source https://api.nuget.org/v3/index.json
