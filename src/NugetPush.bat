45feb38af3e337e9734ad6d557e56f07e43e5742
Peter Kostov <p.kostov@erp.bg>
2026-04-23 16:24:15 +0300

    version bump

--- src/NugetPush.bat
+++ src/NugetPush.bat
@@ -1,4 +1,4 @@
 
-dotnet nuget push ErpNet.Api.Client\bin\Release\ErpNet.Api.Client.26.2.1-beta.nupkg --api-key %1 --source https://api.nuget.org/v3/index.json
+dotnet nuget push ErpNet.Api.Client\bin\Release\ErpNet.Api.Client.27.1.0-beta.nupkg --api-key %1 --source https://api.nuget.org/v3/index.json
 
-dotnet nuget push ErpNet.Api.Client.DomainApi\bin\Release\ErpNet.Api.Client.DomainApi.26.2.1-beta.nupkg --api-key %1 --source https://api.nuget.org/v3/index.json
+dotnet nuget push ErpNet.Api.Client.DomainApi\bin\Release\ErpNet.Api.Client.DomainApi.27.1.0-beta.nupkg --api-key %1 --source https://api.nuget.org/v3/index.json
